292d810556e9952e2fcb0729a3d743fe8b414fac
Oleg Gabrusenok <Oleg_Gabrusenok@rcomext.com>
2016-10-28 14:57:28 +0300

    Bookie server integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      secret: null
+    }
+  }
+
+  componentWillMount() {
+    axios.get(`${process.env.REACT_APP_BOOKIE_SERVER_URL}/secret`)
+      .then(response => {
+        console.log(response);
+        this.setState({
+          secret: response.data
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div>
-        Bookie secret is <b>{process.env.REACT_APP_BOOKIE_SECRET}</b>
+        {this.state.secret ? this.renderSecret()
+                           : <div>'Fetching Bookie Server Secret...'</div>}
       </div>
     );
+  }
+
+  renderSecret() {
+    return (
+      <div>Bookie Server Secret is <b>{this.state.secret}</b></div>
+    )
   }
 }
 
