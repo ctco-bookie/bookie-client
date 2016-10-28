9d632cca74013dfcfe474502d24095d37d3f5a21
trioletas <olegs.gabrusjonoks@gmail.com>
2016-10-28 17:52:53 +0300

    Added gzip compression middleware

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 class App extends Component {
-
   constructor() {
     super();
 
@@ -35,7 +34,7 @@ class App extends Component {
   renderSecret() {
     return (
       <div>Bookie Server Super Secret is <b>{this.state.secret}</b></div>
-    )
+    );
   }
 }
 
