b3aaeaf39dec0e2261934e864a5e765b4828eeed
trioletas <olegs.gabrusjonoks@gmail.com>
2016-10-28 17:52:53 +0300

    Added gzip compression middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const compression = require('compression');
+const app = express();
 
+app.use(compression());
 app.use(express.static(__dirname + '/build'));
 
 app.listen(process.env.PORT || 3000);
