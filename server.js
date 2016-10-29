66e2cacb78d4a898644b88444a7640daaf2f5eaa
trioletas <olegs.gabrusjonoks@gmail.com>
2016-10-29 22:14:41 +0300

    Dropped express server in favor of create-react-app buildpack

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const compression = require('compression');
-const app = express();
-
-app.use(compression());
-app.use(express.static(__dirname + '/build'));
-
-app.listen(process.env.PORT || 3000);
