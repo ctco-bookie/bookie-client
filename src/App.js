38156766ba7ec735875d0838793dfef20bfbd674
Pavels Jelisejevs <jelisejev.p@gmail.com>
2016-10-31 13:26:53 +0200

    initial room page implementation

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      secret: null
-    }
-  }
-
-  componentWillMount() {
-    this.fetchSecret();
-  }
-
-  async fetchSecret() {
-    try {
-      const {data: secret} = await axios.get(`${process.env.REACT_APP_BOOKIE_SERVER_URL}/secret`);
-      this.setState({secret});
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   render() {
     return (
       <div>
-        {this.state.secret ? this.renderSecret()
-                           : <div>Fetching Bookie Server Super Secret...</div>}
+        <h1>Welcome to bookie</h1>
+        {this.props.children}
       </div>
-    );
-  }
-
-  renderSecret() {
-    return (
-      <div>Bookie Server Super Secret is <b>{this.state.secret}</b></div>
     );
   }
 }
