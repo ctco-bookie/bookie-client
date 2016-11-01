434aea84f02dccca5a5ee981b5d7d379f163e386
Pavels Jelisejevs <jelisejev.p@gmail.com>
2016-11-01 12:02:47 +0200

    added page title

--- src/Room.js
+++ src/Room.js
@@ -16,9 +16,14 @@ class Room extends Component {
 
   async loadRoomInfo() {
     const response =  await axios.get(`${process.env.REACT_APP_BOOKIE_SERVER_URL}/calendar/${this.props.params.email.toLowerCase()}`);
+    document.title = this.toCapitalCase(response.data.name);
     this.setState({
       roomInfo: response.data
     })
+  }
+
+  toCapitalCase(str) {
+    return str.replace(/\w\S*/g, (txt) => {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
   }
 
   render() {
