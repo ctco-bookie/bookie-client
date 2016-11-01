d3f8323f815d2fd9a1e3089f3409a50c42c089a8
Pavels Jelisejevs <jelisejev.p@gmail.com>
2016-11-01 13:15:26 +0200

    fixed test

--- src/index.js
+++ src/index.js
@@ -4,18 +4,12 @@ import { Router, Route, hashHistory } from 'react-router';
 import App from './App';
 import Room from './Room';
 import './index.css';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-injectTapEventPlugin();
 
 ReactDOM.render(
-  <MuiThemeProvider>
-    <Router history={hashHistory}>
-      <Route path="/" component={App}>
-        <Route path="room/:email" component={Room}/>
-      </Route>
-    </Router>
-  </MuiThemeProvider>,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <Route path="room/:email" component={Room}/>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
