bc42f892a3e41b947e6564a37aacd215f938b197
Igor Gursky <gdarkey@gmail.com>
2016-11-02 13:12:43 +0200

    Room number added to label

--- src/Room.js
+++ src/Room.js
@@ -60,7 +60,7 @@ class Room extends Component {
   renderRoomInfo(room) {
     return (
       <CardText>
-        <h2 style={{ marginTop: 0 }}>Room {this.toCapitalCase(room.name)}</h2>
+        <h2 style={{ marginTop: 0 }}>Room {this.toCapitalCase(room.name)} ({room.number})</h2>
         <div style={{ float: 'left', background: (room.busy) ? '#FF482C' : '#3ABF78', height: 16, width: 16, borderRadius: 8, marginRight: 8 }}></div>
         <div>{room.busy ? 'Busy' : 'Available for ' + room.availableFor}</div>
       </CardText>
