c6b7aede139ac90ae24fd2e5c351c1a15a86c8a5
Andrew Olenik <olenik.andrew@gmail.com>
2016-11-04 12:09:41 +0200

    Room capacity for other rooms is added

--- src/Check.js
+++ src/Check.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import {Card, CardText, CardActions} from 'material-ui/Card';
 import {Link} from 'react-router';
-import capacityIcon from './svg/capacity.svg';
-import capacityIconMaster from './svg/capacityMaster.svg';
 
 import './Check.css';
 
@@ -12,12 +10,8 @@ class Check extends Component {
 
     return (
       <div>
-        <div>
-          {this.renderRoomCard(masterRoom)}
-        </div>
-        <div
-          style={{marginTop: '40px'}}
-        >
+        <div>{this.renderRoomCard(masterRoom)}</div>
+        <div style={{marginTop: '40px'}}>
           {rooms.map(room => this.renderRoomCard(room))}
         </div>
       </div>
@@ -32,13 +26,16 @@ class Check extends Component {
         <CardText>
           <div className={'room ' + (room.master ? 'room-master' : '') + (!room.availability.busy && room.master ? ' room-master-available' : '')}>
             <h2 className="title">{room.name} ({room.number})</h2>
-            <div className={'indicator ' + (room.availability.busy ? 'busy' : 'available')}></div>
-            <p>{room.availability.busy ? 'Busy till ' + room.availability.availableFrom : 'Available for ' + room.availability.availableFor}</p>
-            <img
-              className="icon"
-              src={room.master ? capacityIconMaster : capacityIcon}
-              alt="capacityIcon"
-            />
+            <div  className="info" >
+              <div className="info-icon info-icon-capacity"></div>
+              <div>{room.capacity}</div>
+            </div>
+            <div className="info">
+              <div className="info-icon">
+                <div className={'indicator ' + (room.availability.busy ? 'busy' : 'available')}></div>
+              </div>
+              <div>{room.availability.busy ? 'Busy till ' + room.availability.availableFrom : 'Available for ' + room.availability.availableFor}</div>
+            </div>
           </div>
         </CardText>
         <CardActions>
